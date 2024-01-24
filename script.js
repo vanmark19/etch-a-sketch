954e982d6b18a14aac8962c710516b4db69a5925
vanmark19 <markisvan19@gmail.com>
2024-01-24 20:52:12 +0200

    loaded on new Ubuntu

---
+++ script.js
@@ -0,0 +1,26 @@
+function createGrid(n){;
+  const grid = document.querySelector('#grid');
+  grid.innerHTML = '';
+  for (let i = 1; i <= n * n; i++){
+    let div = document.createElement('div');
+    div.style.width = `${37.2 / n}rem`
+    div.style.height = `${37.2 / n}rem`
+    grid.appendChild(div);
+  }
+}
+
+createGrid(16);
+
+
+let penColor =  '000000';
+const colorPicker = document.querySelector('#color-picker');
+colorPicker.addEventListener('input', function(e){
+  penColor = e.target.value;
+});
+
+
+const gridPicker = document.querySelector('#change-grid');
+gridPicker.addEventListener('input', function(e){
+  createGrid(e.target.value);
+  document.querySelector('#grid-size').textContent = `${e.target.value} x ${e.target.value}`;
+});
