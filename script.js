63af8fb9f1762554c6e8d6c85f5f282831d902cf
vanmark19 <markisvan19@gmail.com>
2024-01-26 13:47:51 +0200

    Buggy drawing

--- script.js
+++ script.js
@@ -10,9 +10,7 @@ function createGrid(n){;
 }
 
 createGrid(16);
-
-
-let penColor =  '000000';
+let penColor =  '#000000';
 const colorPicker = document.querySelector('#color-picker');
 colorPicker.addEventListener('input', function(e){
   penColor = e.target.value;
@@ -24,3 +22,20 @@ gridPicker.addEventListener('input', function(e){
   createGrid(e.target.value);
   document.querySelector('#grid-size').textContent = `${e.target.value} x ${e.target.value}`;
 });
+let isMouseDown = false;
+document.addEventListener('mousedown', () =>{ isMouseDown = true;
+  console.log(isMouseDown);
+});
+document.addEventListener('mouseup', () => {isMouseDown = false
+  console.log(isMouseDown);});
+document.querySelectorAll('.grid div').forEach(div => {
+  div.addEventListener('mouseenter', e => {
+    console.log('entered');
+    if (isMouseDown)
+    e.target.style.backgroundColor = penColor;
+  });
+});
+
+
+
+
