ef8ef17381d55ce56a79dbbe1a813c3b9ff50e29
vanmark19 <markisvan19@gmail.com>
2024-01-30 22:46:21 +0200

    Made the code tidier

--- script.js
+++ script.js
@@ -1,40 +1,79 @@
+//declare variables and consts
+let penColor =  '#000000';
+const colorPicker = document.querySelector('#color-picker');
+const clearButton = document.querySelector('#clear');
+const gridPicker = document.querySelector('#change-grid');
+
+let isMouseDown = false;
+
+
+
+// declare functions
 function createGrid(n){;
   const grid = document.querySelector('#grid');
   grid.innerHTML = '';
   for (let i = 1; i <= n * n; i++){
     let div = document.createElement('div');
-    div.style.width = `${37.2 / n}rem`
-    div.style.height = `${37.2 / n}rem`
+    div.style.width = `${37.2 / n}rem`;
+    div.style.height = `${37.2 / n}rem`;
+    div.addEventListener('mouseover', e => {
+
+      if (isMouseDown)
+      e.target.style.backgroundColor = penColor;
+    });
     grid.appendChild(div);
   }
 }
 
-createGrid(16);
-let penColor =  '#000000';
-const colorPicker = document.querySelector('#color-picker');
-colorPicker.addEventListener('input', function(e){
+
+function changePenColor(e){
   penColor = e.target.value;
-});
+}
 
 
-const gridPicker = document.querySelector('#change-grid');
-gridPicker.addEventListener('input', function(e){
+function changeGridSize(e){
   createGrid(e.target.value);
   document.querySelector('#grid-size').textContent = `${e.target.value} x ${e.target.value}`;
+}
+
+function clear(e){
+  document.querySelectorAll('#grid div').forEach(div => {
+    div.style.backgroundColor = 'white';
 });
-let isMouseDown = false;
-document.addEventListener('mousedown', () =>{ isMouseDown = true;
-  console.log(isMouseDown);
+}
+
+
+
+
+
+
+//changes the isMouseDown variable
+document.addEventListener('mousedown', e =>{ 
+  if (e.target != document.querySelector('#change-grid'))
+  e.preventDefault();
+  isMouseDown = true;
+
 });
 document.addEventListener('mouseup', () => {isMouseDown = false
-  console.log(isMouseDown);});
-document.querySelectorAll('.grid div').forEach(div => {
-  div.addEventListener('mouseenter', e => {
-    console.log('entered');
-    if (isMouseDown)
-    e.target.style.backgroundColor = penColor;
-  });
 });
+
+
+
+
+// generates the first grid
+createGrid(16);
+
+
+
+//adds the necessary event listeners
+colorPicker.addEventListener('input', changePenColor(e));
+
+gridPicker.addEventListener('input', changeGridSize(e));
+
+clearButton.addEventListener('click', clear(e));
+
+
+
 
 
 
